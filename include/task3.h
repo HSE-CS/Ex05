15b2e5d5187ebb34f6b91096aadfdc26eda35693
Your Name <olga.frolova.2001@bk.ru>
2020-10-26 20:50:37 +0300

    3 task

--- include/task3.h
+++ include/task3.h
@@ -1,1 +1,12 @@
-#pragma once
+template <typename T>
+T minArr(T &arr)
+{
+	T min = 9999999;
+	T* p = &arr;
+	for (int i = 0; i < sizeof(arr); i++)
+	{
+		if (p[i] < min)
+			min = p[i];
+	}
+	return min;
+}
---
+++ src/main3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "task3.h"
+
+int main()
+{
+	float arr[5] = {1.6, 2.8,3.5,4.1,5.9 };
+	std::cout << minArr(*arr) << std::endl;
+	return 0;
+}
