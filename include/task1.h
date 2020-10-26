f94ec8fffe405628ec10da87ad77b9bfab6bdba0
Your NameVadukk <Vadik-Tumakov.com@yandex.ru>
2020-10-26 22:36:03 +0300

    task1

---
+++ include/task1.h
@@ -0,0 +1,17 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+#include <string.h>
+#include <string>
+
+template<class S>
+bool less(S a, S b)
+{
+	return a < b;
+}
+template<> bool less<const char*>(const char* a, const char* b)
+{
+	return strcmp(a, b) < 0;
+}
+
+#endif
