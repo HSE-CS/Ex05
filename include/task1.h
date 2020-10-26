0938ebc873668f95de321a6e55bc0ce7c82db9eb
alexgiving <alexgiving@mail.ru>
2020-10-27 01:36:57 +0300

    update first task

--- include/task1.h
+++ include/task1.h
@@ -5,16 +5,16 @@
 #ifndef EX05_TASK1_H
 #define EX05_TASK1_H
 
-#include <string>
-
-template<typename T>
-bool less(T arg1, T arg2){
-    return arg1 < arg2;
-}
-
-template<>
-bool less<const char *>(const char * arg1, const char * arg2){
-    return strcmp(arg1, arg2) < 0;
-}
+//#include <string>
+//
+//template<typename T>
+//bool less(T arg1, T arg2){
+//    return arg1 < arg2;
+//}
+//
+//template<>
+//bool less<const char *>(const char * arg1, const char * arg2){
+//    return strcmp(arg1, arg2) < 0;
+//}
 
 #endif //EX05_TASK1_H
