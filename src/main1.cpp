f9bd6cdf156cae5d91d00653aeccb27ab22d0539
EgorBuzanov <egor4ek007@gmail.com>
2020-10-26 23:51:02 +0300

    Egor Buzanov

---
+++ src/main1.cpp
@@ -0,0 +1,11 @@
+#include "task1.h"
+
+int main()
+{
+    int a = 1, b = 2;
+    std::cout << less(a, b);
+    char first[] = "Hello";
+    char second[] = "Hello, world!";
+    std::cout << less(first, second);
+    return 0;
+}
