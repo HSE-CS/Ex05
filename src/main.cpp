214951f4006bdc3a0355de79e2b0ecbfabe0f3f5
alexgiving <alexgiving@mail.ru>
2020-10-27 01:26:30 +0300

    update first task

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@
 #include "task3.h"
 
 int main() {
-    std::cout << less(5,7);
-    std::cout << less("five", "seven");
+//  Task_1
+    std::cout << less(6,7) << std::endl;
+    std::cout << less("five", "seven") << std::endl;
+
+// Task_2
+    int arr1[] = {5, 6, 7, 3, 1, -5, -7, -15};
+    std::cout << averageArr(arr1, sizeof(arr1)/sizeof(int)) << std::endl;
+
+// Task_3
+    int arr2[] = {5, 6, 7, 3, 1, -5, -7, -15};
+    std::cout << minArr(arr2) << std::endl;
     return 0;
 }
