e8889b9552eeb5d84c868a6e6f5808dfd16c70b8
KuznetsovMikhail <KuznetsovMikhail>
2020-10-26 22:55:53 +0300

    Кузнецов Михаил первая попытка

---
+++ src/main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "task2.h"
+
+int main()
+{
+    const size_t n = 5;
+    int arr[n] = {4, 7, -3, 5, 2};
+    std::cout << averageArr(arr, n);
+    return 0;
+}
---
+++ src/main3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task3.h"
+
+int main()
+{
+    const size_t n = 5;
+    int arr[n] = {4, 7, -3, 5, 2};
+    std::cout << minArr(arr);
+    return 0;
+}
+
+
