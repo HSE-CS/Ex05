a9579396dfa5c3c206724ebd9c5f16940c5e008d
alexgiving <alexgiving@mail.ru>
2020-10-27 02:19:41 +0300

    solve problem with compiling

---
+++ src/main1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Алексей Трутнев on 27.10.2020.
+//
+
+#include <iostream>
+#include "task1.h"
+
+int main() {
+    std::cout << less(6,7) << std::endl;
+    std::cout << less("five", "seven") << std::endl;
+    return 0;
+}
