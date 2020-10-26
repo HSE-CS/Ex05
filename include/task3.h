c5ecbee695d4c7d6906cf44e3b211e61051377aa
kisozinov <71232712+kisozinov@users.noreply.github.com>
2020-10-26 21:53:00 +0300

    replaced INT_MAX by 2147483647

--- include/task3.h
+++ include/task3.h
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <limits>
+#include <bits/stdc++.h>
 
 template <typename T, size_t n>
 T minArr(T (& arr)[n])
 {
 	size_t size = sizeof(arr) / sizeof(arr[0]);
-	T min = INT_MAX;
+	T min = 2147483647;
 	for (size_t i = 0; i < size; i++) {
 		if (arr[i] < min) min = arr[i];
 	}
