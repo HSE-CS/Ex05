def75277e041846fefe073fb045a9f9dcc534cd8
HabKaffee <neknaum1@gmail.com>
2020-10-26 22:53:56 +0300

    fix16

--- include/task2.h
+++ include/task2.h
@@ -6,7 +6,7 @@ template<typename T, typename T2> T averageArr(T* arr, T2 n) {
 	T average = { 0 };
 
 	for (size_t i = 0; i < n; ++i) {
-		average += array[i];
+		average += arr[i];
 	}
 	return average/n;
 }
