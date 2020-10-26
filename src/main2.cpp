8e285c2b1cc3ab358404d992a8da92f09d253307
HabKaffee <neknaum1@gmail.com>
2020-10-26 23:43:46 +0300

    3rd attempt 6.0

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,10 +1,10 @@
-//#include "task2.h"
-//#include <iostream>
-//
-//#define n 6 
-//
-//int main() {
-//	int val[n] = { 4, 3, 2, 2, 2, -1 };
-//	std::cout<< ::averageArr<int>(val, n)<<std::endl;
-//	return 0;
-//}
+#include "task2.h"
+#include <iostream>
+
+#define n 6 
+
+int main() {
+	int val[n] = { 4, 3, 2, 2, 2, -1 };
+	std::cout<< ::averageArr<int>(val, n)<<std::endl;
+	return 0;
+}
