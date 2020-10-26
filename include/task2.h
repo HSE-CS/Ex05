7164f22f64ae9b8567470e11dc3d4b9d252fdcb5
alexgiving <alexgiving@mail.ru>
2020-10-26 21:31:58 +0300

    second task

---
+++ include/task2.h
@@ -0,0 +1,18 @@
+//
+// Created by Алексей Трутнев on 26.10.2020.
+//
+
+#ifndef EX05_TASK2_H
+#define EX05_TASK2_H
+
+#include <iostream>
+
+template <typename T>
+float averageArr(const T * array, int size){
+    float sum = 0.;
+    for (size_t i = 0; i < size; ++i)
+        sum += array[i];
+    return sum / size;
+}
+
+#endif //EX05_TASK2_H
