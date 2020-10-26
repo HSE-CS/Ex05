b1e70232b69fb89676458de97b1a87bd4546d5a0
Irina <pasmanikirina29@gmail.com>
2020-10-26 22:32:22 +0300

    task 1

---
+++ src/main1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "task1.h"
+
+int main() {
+	bool f = 0;
+
+	int a = 5, b = 8;
+	f = less(a,b);
+	std::cout << f;
+
+
+	int a = 15, b = 6;
+	f = less(a, b);
+	std::cout << f;
+	return 0;
+}
