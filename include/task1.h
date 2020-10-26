9e29e92d2ff93f9c1112d1b4168372ffe290a505
NikDemoShow <Nixon2001@yandex.ru>
2020-10-26 22:44:29 +0300

    First try"
    ``
    
    fff
    f
    f
    f
    f
    f
    f
    qqqqq`````````````````
    `
    git
    end
    break
    ``````~~~~~~~~~~~~~~qsqqqq11
    
    "f"
    "

---
+++ include/task1.h
@@ -0,0 +1,17 @@
+#ifndef TASK_1
+#define TASK_1
+template< typename T>
+bool less(T a, T b) {
+	if (a - b < (T)(0.000000)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<> bool less<const char*>(const char* a, const char* b) {
+	return strcmp(a, b) < 0;
+}
+
+#endif
