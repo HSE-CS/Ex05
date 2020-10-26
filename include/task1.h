a48ce4440da1464b2d2e2f642b16e8439fea65e0
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-10-26 22:55:53 +0300

    first task

---
+++ include/task1.h
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <string>
+
+tempalate <class T>
+bool less(T a, T b)
+{
+
+  return (a < b);
+}
+
+template <>
+bool less(const char* a, const char* b)
+{
+    return strcmp(a, b) < 0;
+}
