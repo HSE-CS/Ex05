8041b26ee8b45ffa3e5f2bc47a3df069cab527a8
HabKaffee <neknaum1@gmail.com>
2020-10-26 22:43:58 +0300

    Fix14

--- include/task2.h
+++ include/task2.h
@@ -1,9 +1,9 @@
 #ifndef TASK2_H
 #define TASK2_H
 
-template <typename T, size_t n> T averageArr(T(&array)[n], int n) {
+template<typename T, typename T2> T averageArr(T* arr, T2 n) {
 	
-	T average = { 0 };
+	T2 average = { 0 };
 
 	for (size_t i = 0; i < n; ++i) {
 		average += array[i];
