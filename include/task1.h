6fe9a7fccdd5a3bca6b32d50a07a482330df9250
Your Name <olga.frolova.2001@bk.ru>
2020-10-26 21:51:26 +0300

    variat 2

--- include/task1.h
+++ include/task1.h
@@ -6,10 +6,24 @@ template <typename T>
 bool less(T a, T b)
 {
 	if (a < b)
+	{
 		return true;
+	}
+	else
+	{
+		return false;
+	}
+
 }
-template<>bool less<char*>(char* a,  char* b)
+template<>bool less<char*>(char* a, char* b)
 {
 	if (strcmp(a, b) < 0)
+	{
 		return true;
+	}
+	else
+	{
+		return false;
+	}
+
 }
