82c3061876a84d848c5cb1ee0bfe29613b0ec0ae
HabKaffee <neknaum1@gmail.com>
2020-10-26 19:09:37 +0300

    Task2 done

---
+++ src/main2.cpp
@@ -0,0 +1,10 @@
+//#include "task2.h"
+//#include <iostream>
+//
+//const size_t n = { 10 };
+//
+//int main() {
+//	int val[n] = { 4, 3, 2, 2, 2, -1, 0,0,0,1000 };
+//	std::cout<< averageArr(val, n)<<std::endl;
+//	return 0;
+//}
