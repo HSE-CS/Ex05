ac92b725f5edc0f0093ac48e18717aa867a0efc4
VarginDimitry <dimitry.vargin@yandex.ru>
2020-10-28 00:15:57 +0300

    1.0

--- include/task3.h
+++ include/task3.h
@@ -4,6 +4,12 @@
 #include <iostream>
 using namespace std;
 
-
+template <typename T, size_t arr_size>
+T minArr(T (&arr)[arr_size]){
+    T min = arr[0];
+    for (int i = 0; i < arr_size; ++i)
+        min = arr[i] < min ? arr[i] : min;
+    return min;
+}
 
 #endif //TASK1_TASK3_H
