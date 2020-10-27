16edb652168b7731ae27ff39f00fa9f0fcb79064
Urtyukov Ilya <urtilya@gmai.com>
2020-10-27 08:30:42 +0300

    2

--- include/task3.h
+++ include/task3.h
@@ -8,8 +8,7 @@ using namespace std;
 template <typename T, size_t n>
 T minArr(T (& arr)[n])
 {
-	size_t size = sizeof(arr) / sizeof(arr[0]);
-	T min = INT_MAX;
+	T min = arr[0];
 	for (size_t i = 0; i < n; i++) {
 		if (arr[i] < min) min = arr[i];
 	}
