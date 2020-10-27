f1c324df065c4a6038f3b1c21889938148d71bc2
Ilya <ilya.tatsenko@yandex.ru>
2020-10-27 23:55:11 +0300

    3

--- include/task1.h
+++ include/task1.h
@@ -2,7 +2,7 @@
 #include<iostream>
 
 template< typename T>
-bool less_(const T x, const T y)
+bool less(const T x, const T y)
 {
 
 	if (x > y) return false;
@@ -10,7 +10,7 @@ bool less_(const T x, const T y)
 
 }
 template<> 
-bool less_(const char* x, const char* y)
+bool less(const char* x, const char* y)
 {
 	int i;
 	for ( i = 0; x[i] != '\0' || y[i] != '\0'; i++)
@@ -20,7 +20,7 @@ bool less_(const char* x, const char* y)
 		if (x[i] < y[i]) return true;
 
 	}
-	if (x[i] != '\0 ' && y[i] == '\0') return false;
+	if (x[i] != '\0' && y[i] == '\0') return false;
 	if (y[i] != '\0' && x[i] == '\0') return true;
 	//if (x[i] == '\0' && y[i] == '\0') return 0;
 }
--- include/task3.h
+++ include/task3.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include<iostream>
 
 template < typename T >
 auto minArr( T &mas)
