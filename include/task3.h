e5cb8a4e1b49d8dad6d2d8489fe99ae5ea075bcb
Nik <50590478+stifeevn@users.noreply.github.com>
2020-10-28 00:39:39 +0500

    fixinc2

--- include/task3.h
+++ include/task3.h
@@ -3,7 +3,7 @@
 template <typename T, size_t sizeArr>
 T minArr(T(&arr)[sizeArr]) {
     T res_min = arr[0];
-    for (auto num : arr) {
+    for (T num : arr) {
         if (num < res_min)
             res_min = num;
     }
