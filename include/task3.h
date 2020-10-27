235db6327424e11e379ab731ce3821f400172f0b
m_khorosh <marina.khoroshilova@gmail.com>
2020-10-27 15:15:24 +0300

    fix size in template

--- include/task3.h
+++ include/task3.h
@@ -1,7 +1,7 @@
 #ifndef TASK1_TASK3_H
 #define TASK1_TASK3_H
 
-template<class T, std::size_t N>
+template<class T, size_t N>
 T minArr(T (&mas)[N]){
     T min = mas[0];
     for (int i=1;i<N;i++){
