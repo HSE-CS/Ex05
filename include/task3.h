a9155f298af2fec16d2ddb9dc1eaa23f665e932b
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-10-27 17:01:55 +0300

    fourth

--- include/task3.h
+++ include/task3.h
@@ -4,7 +4,7 @@ template<typename T>
 
 T minArr(T arr) {
 	int len = sizeof(arr) / sizeof(arr[0]);
-	double min{ INT_MAX };
+	int min{ INT_MAX };
 	for (size_t i = 0; i < len; i++)
 	{
 		if (arr[i] < min)
