4c4f1ee6d6c30c55c1a25335ceed74f073d9a025
Ramazan <ramazankka220@gmail.com>
2020-10-27 21:51:42 +0300

    fixing problems

--- include/task1.h
+++ include/task1.h
@@ -6,18 +6,14 @@
 #include <cstring>
 
 template< typename T >
-bool less(T& first, T& second) //T - тип, указанный в параметре шаблона
+bool less(T first, T second) //T - тип, указанный в параметре шаблона
 {
     return first < second;
 }
 
- template<> 
- bool less(char& first, char& second)
- {
-     return strcmp(*first, *second);
- }
-
-
-
-
+template<>
+bool less(const char* first, const char* second)
+{
+    return strcmp(first, second);
+}
 
