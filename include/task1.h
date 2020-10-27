4c44665067af4ef27dc442c63921e34547240402
m_khorosh <marina.khoroshilova@gmail.com>
2020-10-27 12:29:10 +0300

    initial commit

---
+++ include/task1.h
@@ -0,0 +1,14 @@
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+
+template<class T>
+bool less(T a, T b){
+    return a<b;
+}
+
+template<>
+bool less<const char*>(const char* a, const char* b){
+    return strcmp(a, b) < 0;
+}
+
+#endif
---
+++ src/main1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "task1.h"
+
+int main(){
+    std::cout << less("baab","abba");
+    return 0;
+}
