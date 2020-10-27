879c6978975900e85804979de035238585fa725b
Irina <pasmanikirina29@gmail.com>
2020-10-27 11:10:22 +0300

    task 1,2

--- include/task2.h
+++ include/task2.h
@@ -3,7 +3,7 @@
 template < typename T >
 T averageArr(T* array[], int size) {
 	T& sum = 0;
-	for (int i = 0; i < size) {
+	for (int i = 0; i < size; ++i) {
 		sum += array[i];
 	}
 	return sum;
--- src/main2.cpp
+++ src/main2.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include "task2.h"
 
+#define SIZE 3
+
 int main() {
-	T s = 0;
+	int s1 = NULL;
+	int arr[SIZE] = { 1,2,3 };
+	s1 = averageArr(arr, SIZE);
+	std::cout << s1;
 
-	int arr = {1,2,3};
-	s = averageArr(arr);
-	std::cout << s;
-
-	int arr = { 5,7,9 };
-	s = averageArr(arr);
-	std::cout << s;
+	/*char s2 = NULL;
+	int size2 = 3;
+	char arr[size2] = { '5','7','9' };
+	s2 = averageArr(arr);
+	std::cout << s;*/
 
 	return 0;
 }
