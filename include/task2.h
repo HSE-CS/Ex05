5506848d02dbed031a7b3525bcdf56c98de5049f
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-10-27 17:07:08 +0300

    fifth

--- include/task2.h
+++ include/task2.h
@@ -2,7 +2,7 @@
 // #include <time.h>
 template<typename T>
 
-T averageArr(T arr, T len) {
+double averageArr(T arr, T len) {
 	// int len = sizeof(arr) / sizeof(arr[0]);
 	double result{ 0 };
 	for (size_t i = 0; i < len; i++)
--- include/task3.h
+++ include/task3.h
@@ -2,7 +2,7 @@
 template<typename T>
 #define INT_MAX 2147483647
 
-T minArr(T arr) {
+int minArr(T arr) {
 	int len = sizeof(arr) / sizeof(arr[0]);
 	int min{ INT_MAX };
 	for (size_t i = 0; i < len; i++)
