3bf27b09746047e42b418f5a6cfba4c796219c1e
Nik <50590478+stifeevn@users.noreply.github.com>
2020-10-28 00:55:36 +0500

    fix

--- src/main1.cpp
+++ src/main1.cpp
@@ -3,17 +3,23 @@
 
 
 int main() {
-	int first_int = 5;
-	int second_int = 3;
+	int x = 10;
+	int y = 100;
+	const char* str1 = "template";
+	const char* str2 = "cpp";
+	std::cout << less(str1, str2) << std::endl;
+	std::cout << less(a, b) << std::endl;
+	//int first_int = 5;
+	//int second_int = 3;
 
-	double first_double = 15.0;
-	double second_double = 25.7;
+	//double first_double = 15.0;
+	//double second_double = 25.7;
 
-	std::string first_str = "cpp";
-	std::string second_str = "template";
+	//std::string first_str = "cpp";
+	//std::string second_str = "template";
 
-	std::cout << less(first_int, second_int) << std::endl;
-	std::cout << less(first_double, second_double) << std::endl;
-	std::cout << less(first_str, second_str) << std::endl;
+	//std::cout << less(first_int, second_int) << std::endl;
+	//std::cout << less(first_double, second_double) << std::endl;
+	//std::cout << less(first_str, second_str) << std::endl;
 	return 0;
 }
