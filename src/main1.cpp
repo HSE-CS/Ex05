546aefbe04f293ce471ba56ed577b71bc638bc8e
DenisKabanov <dskabanov@edu.hse.ru>
2020-10-27 21:23:34 +0300

    b71

---
+++ src/main1.cpp
@@ -0,0 +1,8 @@
+#include "task1.h"
+
+int main() 
+{
+	cout << ::less(11, 40) << endl;
+	cout << ::less('a', 'c')<< endl;
+	cout << ::less("aa", "ab") << endl;
+}
---
+++ src/main2.cpp
@@ -0,0 +1,8 @@
+#include "task2.h"
+
+int main() 
+{
+	const unsigned int n = 5;
+	int arr[n]{ 1, 3, 4, 5, 6};
+	cout << averageArr(arr, n) << endl;
+}
