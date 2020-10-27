bdbec7635b7cde430b3e89e24934a7e27640861e
Karcevamd <mashkarcev@gmail.com>
2020-10-27 16:33:57 +0300

    19AMI Masha Kartseva

--- include/task2.h
+++ include/task2.h
@@ -2,7 +2,7 @@
 #define TASK2_H
 
 template <typename T>
-T averageArr(T& arg, int size ) {
+T averageArr(T* arg, int size ) {
 	T sum = 0;
 	for (int i = 0; i < size; i++) {
 		sum = sum + arg[i];
--- src/main1.cpp
+++ src/main1.cpp
@@ -2,6 +2,7 @@
 #include "task1.h"
 
 int main() {
-   
+    std::cout << less(10, 20);
+    std::cout << less("acb", "sdggrh");
     return 0;
 }
