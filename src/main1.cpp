691c816f17d90d21658132d4a7a2a03f2a2c309b
EgorBodrov <bodroff.alexeevich@yandex.ru>
2020-10-27 13:36:44 +0300

    b32 Bodrov Egor

---
+++ src/main1.cpp
@@ -0,0 +1,11 @@
+#include "task1.h"
+
+int main()
+{
+	/*std::cout << less(1, 10) << std::endl;
+	const char* a = "aab";
+	const char* b = "aac";
+	std::cout << less(a, b) << std::endl;*/
+
+	return 0;
+}
