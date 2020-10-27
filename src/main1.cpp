71fcf2b3487f6a1ae3ad865085f805e38909d517
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-27 21:04:28 +0300

    b75

---
+++ src/main1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include "task1.h"
+
+
+int main() {
+
+    char a[] = { "abba" };
+    char b[] = { "baab" };
+
+    std::cout << less(a, b);
+
+    return 0;
+}
