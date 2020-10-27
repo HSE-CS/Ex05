5aea6b780b82314b40e6964de5fe3163ebc530a5
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-27 10:38:59 +0300

    TEST BUILD
    ONLY FOR TESTING

--- include/task1.h
+++ include/task1.h
@@ -1,16 +1,23 @@
 //
-// Created by Алексей Трутнев on 26.10.2020.
+//  Тревис отказывается собирать мой проект, я решил проверить его работоспособность на чужом коде. 
 //
 
-#include <string>
+#pragma once
+ #include<cstring>
+ #include<iostream>
 
-template<class T>
-bool less(T arg1, T arg2){
-    return arg1 < arg2;
-}
+ template<typename T>
+ bool less(T a, T b) {
+ 	return (a < b) ? true : false;
+ }
 
-template<>
-bool less<const char *>(const char * arg1, const char * arg2){
-    return strcmp(arg1, arg2) < 0;
-}
+ template<>
+ bool less<char*>(char* a, char* b) {
+
+ 	if (strcmp(a, b) >= 0) {
+ 		return false;
+ 	}else {
+ 		return true;
+ 	}
+ } 
 
