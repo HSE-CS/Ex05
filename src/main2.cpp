0ea98e93e12a9898fff60c7c1a7e38363abeaceb
Vladislav-Kanev <vokanev@edu.hse.ru>
2020-10-27 19:21:41 +0300

    some changes after the lecture attempt 2

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,14 +1,14 @@
-////#include"../include/task2.h"
-//#include <iostream>
-//#include "task2.h"
-//
-//
-//int main() {
-//	const size_t n = 6;
-//	int a[n]{ 10, 2, 4, 8, 9, 5 };
-//	std::cout << averageArr(a, n) << '\n';
-//
-//	float b[n]{ 10.0, 2., 4., 8., 9., 5. };
-//	std::cout << averageArr(a, n);
-//	return 0;
-//}
+//#include"../include/task2.h"
+#include <iostream>
+#include "task2.h"
+
+
+int main() {
+	const size_t n = 6;
+	int a[n]{ 10, 2, 4, 8, 9, 5 };
+	std::cout << averageArr(a, n) << '\n';
+
+	float b[n]{ 10.0, 2., 4., 8., 9., 5. };
+	std::cout << averageArr(a, n);
+	return 0;
+}
