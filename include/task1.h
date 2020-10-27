7b1956ca643a50c0dbef212a12d76da4f7ef6cde
Elizaveta Drobot <lizunchik7@bk.ru>
2020-10-28 02:14:04 +0300

    task1

--- include/task1.h
+++ include/task1.h
@@ -9,13 +9,3 @@ template<> bool less<const char*>(const char* a, const char* b)
     return strcmp(a, b) < 0;
 }
 
-template<class T> T averageArr(T& arg, unsigned int n)
-{
-    
-    T sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum = sum + arg[i];
-    }
-    return sum / n;
-    
-}
--- src/main1.cpp
+++ src/main1.cpp
@@ -13,11 +13,5 @@ int main() {
     int a = 5, b = 6;
     std::cout << less(a, b);
 
-
-    const size_t n = 6;
-    int val[n]{ 4, 3, 2, 2, 2, -1 };
-    std::cout << averageArr(val, n);
-
-
     return 0;
 }
