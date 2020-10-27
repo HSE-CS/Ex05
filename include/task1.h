69fb1129c9dc8fbb28daf611cc102ed3370d0c5a
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-10-27 13:53:28 +0300

    task1 done

---
+++ include/task1.h
@@ -0,0 +1,15 @@
+#ifndef TASK1_H
+#define TASK1_H
+#include <string.h>
+
+template<typename T> bool less(T a, T b)
+{
+	return (a < b);
+}
+
+template<> bool less<const char*>(const char* a, const char* b)
+{
+	return strcmp(a, b) < 0;
+}
+
+#endif
