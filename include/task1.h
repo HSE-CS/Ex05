405eddd6afa2bfb853ec907a8778db3d08f72865
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-27 10:33:55 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -2,9 +2,6 @@
 // Created by Алексей Трутнев on 26.10.2020.
 //
 
-#ifndef EX05_TASK1_H
-#define EX05_TASK1_H
-
 #include <string>
 
 template<class T>
@@ -17,4 +14,3 @@ bool less<const char *>(const char * arg1, const char * arg2){
     return strcmp(arg1, arg2) < 0;
 }
 
-#endif //EX05_TASK1_H
