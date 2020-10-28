ebb669c5c5f1d6ddaef32371bd8323150d9acc1e
Roma Balayan <roma1balayan@gmail.com>
2020-10-28 09:42:36 +0300

    First task v1.2

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,15 +1,15 @@
 #include "../include/task1.h"
 #include <iostream>
 
-using namespace std;
+//using namespace std;
 
 int main()
 {
 	int a = 0;
 	int b = 5;
-	cout << Ex05::less((char*)("aaa"), (char*)("aab")) << endl;
-	cout << Ex05::less(a, b) << endl;
-	cout << Ex05::less(0.6, 0.5) << endl;
+	std::cout << less((char*)("aaa"), (char*)("aab")) << std::endl;
+	std::cout << less(a, b) << std::endl;
+	std::cout << less(0.6, 0.5) << std::endl;
 
 	return 0;
 }
