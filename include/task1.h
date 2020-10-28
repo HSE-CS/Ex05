4bdc4e65ab5c6e0183dde0b70b41ca99e114719b
PolinaZharavina <pdzharavina@edu.hse.ru>
2020-10-28 13:26:51 +0300

    Test 1 task1

---
+++ include/task1.h
@@ -0,0 +1,14 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+#include <cstring>
+#include <string>
+
+template<typename T> bool less(T a, T b) {
+	if (a < b)
+		return true;
+	else
+		return false;
+}
+
+#endif
