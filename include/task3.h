9dc60bb9148aa8cbdaea307f7f1dee62c51fed8b
Nikita Kasyanov <kasyanikita@gmail.com>
2020-10-28 11:19:44 +0300

    Second attempt

--- include/task3.h
+++ include/task3.h
@@ -1,13 +1,13 @@
 #ifndef TASK1_TASK3_H
 #define TASK1_TASK3_H
 
-#include <algorithm>
-
 template<typename T, size_t n>
 T minArr(T (&a)[n]) {
     T min = a[0];
     for(size_t i = 0; i < n; ++i) {
-        min = std::min(min, a[i]);
+        if (min > a[i]) {
+            min = a[i];
+        }
     }
     return min;
 }
