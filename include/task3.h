52693a31a9ae67fb4362cdc8b0b1790694004a6b
Alyona Uskova <aluskova01@yandex.ru>
2020-10-28 20:05:15 +0300

    Uskova b27

--- include/task3.h
+++ include/task3.h
@@ -4,14 +4,14 @@
 #include <string.h>
 #include <iostream>
 
-template <typename T> int minArr(T arr)
+template <typename T, size_t N> int minArr(T (&arr)[N])
 {
-	int n = sizeof(arr) / sizeof(T);
+	int n = sizeof(arr) / sizeof(int);
 	int min = arr[0];
 	for (int i = 0; i < n; i++)
 	{
-		if (i < min)
-			min = i;
+		if (arr[i] < min)
+			min = arr[i];
 	}
 	return min;
 }
