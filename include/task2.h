7e86daf09bab806a58bd5f15300eeb4c049b1849
Urtyukov Ilya <urtilya@gmai.com>
2020-10-29 13:12:51 +0300

    return

--- include/task2.h
+++ include/task2.h
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+#ifndef Task2_H
+#define Task2_H
+#include <iostream>
+
+using namespace std; 
+
+
+template <typename T>
+double averageArr(T arr, size_t size_arr)
+{
+	T sum = 0;
+	for (size_t i = 0; i < size_arr; i++)
+		sum += arr[i];
+	return sum / size_arr;
+} 
+#endif
+=======
 #include <iostream>
 
 template <typename T>
@@ -8,3 +26,4 @@ double averageArr(T ptr, int size_arr)
 		sum += ptr[i];
 	return sum / size_arr;
 } 
+>>>>>>> origin/b112
--- src/main1.cpp
+++ src/main1.cpp
@@ -12,7 +12,11 @@ int main (){
 	char s2[] = "gaaaaame";
 	bool res = less(*s1,*s2);
 	cout << res<<endl;
+<<<<<<< HEAD
+	int x=5,y=3;
+=======
 	int x=5,y=2;
+>>>>>>> origin/b112
 	res=less(x,y)
 	cout<<res;
 	return 0;
