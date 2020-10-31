c206dafb54d85a06aaf4f16c6219d207e40a6208
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-10-31 15:31:13 +0300

    generic fun initial commit

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,4 +1,15 @@
+
 /*
  * @author Stanislav Stoianov
  */
+
+#include <iostream>
+#include "../include/task1.h"
+
+int main() {
+    std::cout << less(26, 5) << std::endl;
+    std::cout << less("f", "g") << std::endl;
+    std::cout << less("abba", "baab") << std::endl;
+    return 0;
+}
 
