6539e4302a423c2a2c1c77d8cad89ae4d5139a61
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-10-31 15:34:22 +0300

    generic fun 2 initial commit

--- include/task2.h
+++ include/task2.h
@@ -5,4 +5,13 @@
 #ifndef TASK1_TASK2_H
 #define TASK1_TASK2_H
 
+#include <iostream>
+
+template<typename T>
+double averageArr(T arr, const size_t size) {
+    double sum = 0;
+    for (auto i = 0; i < size; i++) sum += *arr++;
+    return sum /= size;
+}
+
 #endif
--- src/main2.cpp
+++ src/main2.cpp
@@ -2,3 +2,11 @@
  * @author Stanislav Stoianov
  */
 
+#include <iostream>
+#include "../include/task2.h"
+
+int main() {
+    double arr[]{535, -265, -282, 251, 257, -326, 437, -372};
+    std::cout << averageArr(arr, 8);
+    return 0;
+}
