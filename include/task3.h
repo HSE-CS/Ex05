f61f24d9d03f8eb742656081f599205139330d34
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-10-31 15:39:33 +0300

    generic fun 3 initial commit

--- include/task3.h
+++ include/task3.h
@@ -5,4 +5,11 @@
 #ifndef TASK1_TASK3_H
 #define TASK1_TASK3_H
 
+template<typename T>
+double minArr(T &arr) {
+    auto min = *arr;
+    for (auto item : arr) if (min > item) min = item;
+    return (double) min;
+}
+
 #endif
