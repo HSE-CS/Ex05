45eda6d04d5ffb92af076f676b1b4a6b1dbf8477
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2020-11-01 13:49:27 +0300

    b103

--- src/main3.cpp
+++ src/main3.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main()
 {
 	const int size = size_world;
-	setlocale(0, "ru-RU");
 	int my_mass[size] = { 8,4,3,4,5,2,0,7,5,1 };
 	cout <<" min value: " << minArr(my_mass) <<endl;
 }
