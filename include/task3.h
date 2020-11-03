98e0cc7863d2d2a233c2417ea6d3f468e501a2fe
LexaLexarLexarex <idlexar@gmail.com>
2020-11-03 06:42:20 +0300

    Sorry, for cheating. Never again

--- include/task3.h
+++ include/task3.h
@@ -6,7 +6,7 @@ template <class T, size_t size> T minArr(T(&arr)[size])
     T low = arr[0];
     for (T item : arr) 
     {
-        if (low > item) min = item;
+        if (low > item) low = item;
     }
     return low;
 }
