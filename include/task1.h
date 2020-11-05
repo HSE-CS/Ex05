d7723253fab4acce563dbcea4d416e98bbd92cb7
Alexander Grachev <s89519011368@yandex.ru>
2020-11-05 16:08:17 +0300

    task1

---
+++ include/task1.h
@@ -0,0 +1,31 @@
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+
+#include <iostream>
+
+template<typename T>
+bool _max(T a, T b){
+    if (a > b)
+        return true;
+    else
+        return false;
+
+}
+
+template<>
+bool _max(char* a, char* b){
+    int i=0;
+    while(a[i] != '\0' || b[i] != '\0'){
+        if (a[i] == b[i]){
+            i++;
+            continue;
+        }
+        if (a[i] > b[i])
+            return true;
+        else
+            return false;
+    }
+}
+
+
+#endif //TASK1_TASK1_H
