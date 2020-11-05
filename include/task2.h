9e28c44e0b8896f4f870a55df8c707185aa99401
Alexander Grachev <s89519011368@yandex.ru>
2020-11-05 16:38:37 +0300

    task1 2

--- include/task2.h
+++ include/task2.h
@@ -1,4 +1,14 @@
 #ifndef TASK1_TASK2_H
 #define TASK1_TASK2_H
 
+template <typename T>
+    double averageArr(T *arr, unsigned int size){
+
+    T sum = 0;
+    for (unsigned int i = 0; i < size; i++){
+        sum += arr[i];
+    }
+    return (double)sum / size;
+}
+
 #endif //TASK1_TASK2_H
