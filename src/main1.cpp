011f95dba05a5359a54f2d315913f6161bb4fd34
__dolt_coder__ <ihackit595@gmail.com>
2020-11-07 15:35:24 +0300

    first task complete

---
+++ src/main1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+#include "../include/task1.h"
+// using namespace std;
+
+
+int main() {
+  std::cout<<less(2,1)<<std::endl;
+  char *a = "123";
+  char *b = "1234";
+  std::cout<<less(a,b)<<std::endl;
+  return 0;
+}
+
