d4783052bb686ea37d59526f4557ee36dfc67112
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2020-11-08 16:46:31 +0300

    fix

--- include/task1.h
+++ include/task1.h
@@ -5,14 +5,14 @@
 
 template<class T> bool less(T a, T b) 
 {
-    if a < b
+    if(a < b)
         return 1;
     else
         return 0; 
 }
 template<> bool less<const char*>(const char* a, const char* b)
 {
-    if strcmp(a, b) < 0
+    if(strcmp(a, b) < 0)
         return 1;
     else
         return 0;
