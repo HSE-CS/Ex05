d145538e3f59c8020fcd00ebbd5890a0515f18a6
wityat <ttrttr449@gmail.com>
2020-11-10 02:42:27 +0300

    task3

--- include/task3.h
+++ include/task3.h
@@ -4,5 +4,13 @@
 
 #ifndef TASK1_TASK3_H
 #define TASK1_TASK3_H
+#include <cstring>
 
+template<typename T, int SIZE>
+int minArr(T (& array) [SIZE]) {
+    int min = array[0];
+    for (int i = 0; i < SIZE; ++i)
+        if (array[i] < min)  min = array[i];
+    return min;
+}
 #endif //TASK1_TASK3_H
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,4 +1,11 @@
-//
-// Created by victor on 10.11.2020.
-//
+#include <iostream>
+#include "task3.h"
 
+using namespace std;
+
+int main()
+{
+    int arr[5] = {43,53,2,450,10};
+    cout << minArr(arr);
+    return 0;
+}
